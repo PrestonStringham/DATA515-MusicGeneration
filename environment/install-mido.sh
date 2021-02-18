8ef5f1c5262af2d9a1a6820066fbf2ccab712061
Carl Parker <carljparker@fb.com>
2021-02-18 11:08:24 -0800

    Fix environment names

---
+++ environment/install-mido.sh
@@ -0,0 +1,11 @@
+
+#
+# The `activate` command is idempotent, which means that if you activate
+# an environment which is already activated, nothing changes.
+#
+conda activate music-gen-515
+pip install mido
+
+
+# --- END --- #
+
