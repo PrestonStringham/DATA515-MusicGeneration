d082eb2041387659768fe596f93adbc5dfd33b76
Carl Parker <carljparker@fb.com>
2021-02-24 19:25:37 -0800

    Evidently, you can't run conda activate from inside a Bash script...sigh

--- environment/install-magenta.sh
+++ environment/install-magenta.sh
@@ -1,9 +1,4 @@
 
-#
-# The `activate` command is idempotent, which means that if you activate
-# an environment which is already activated, nothing changes.
-#
-conda activate music-gen-515
 pip install magenta
 
 
