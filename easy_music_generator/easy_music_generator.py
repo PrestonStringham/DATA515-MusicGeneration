67413a5cda5a155ad5d16d8d47166ab424d6a0d4
andreiabds <andreia.bdsodre@gmail.com>
2021-03-08 21:06:36 -0800

    polyphony_rnn polyphony mag

--- easy_music_generator/easy_music_generator.py
+++ easy_music_generator/easy_music_generator.py
@@ -18,18 +18,16 @@ class EasyMusicGenerator:
         preg = pg.Pregenerator()
         primer_melody = preg.generate_primer_melody(note_matrix, self.bars) 
         #primer_string = ", ".join(primer_melody)
+        
         primer_string = str(primer_melody)
-        print(type(primer_string))
-        for i in primer_string:
-            print(type(i))
-       
         backing_chords = preg.generate_backing_chords(chord_matrix, self.bars)
         backing_chords_str = ""
         for chord in backing_chords:
             backing_chords_str += chord + " "
-        BUNDLE_PATH = 'chord_pitches_improv.mag'
-        CONFIG = 'basic_improv'
-        command = f'improv_rnn_generate --config={CONFIG} --bundle_file={BUNDLE_PATH} --output_dir=/temp --num_outputs=10 --primer_melody="[56, 64, 40]" --backing_chords="A D G E" --render_chords'
+       
+        BUNDLE_PATH = 'polyphony_rnn.mag'
+
+        command = f'polyphony_rnn_generate --bundle_file={BUNDLE_PATH}​​​​ --output_dir=/tmp/polyphony_rnn/generated2 --num_outputs=10 --num_steps=128 --primer_melody={primer_string}​​​​ --condition_on_primer=true --inject_primer_during_generation=false'
 
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
         process.wait()
