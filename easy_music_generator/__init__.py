cf079e958c412ffde11958e9c1afdaa0557ce2a1
Christie Gan <chrisleegan@gmail.com>
2021-03-08 19:47:22 -0800

    fixing pylint's errors

--- easy_music_generator/__init__.py
+++ easy_music_generator/__init__.py
@@ -1,4 +1,6 @@
 """Top-level __init__.py for EasyMusicGenerator
 """
-from preprocessor import *
+from preprocessor.chord_distribution import ChordDistribution, NoNotesFoundException
+from preprocessor.note_distribution import NoteDistribution
+from preprocessor.preprocessor import Preprocessor, NoFilesFoundException
 from .__version import __version__
