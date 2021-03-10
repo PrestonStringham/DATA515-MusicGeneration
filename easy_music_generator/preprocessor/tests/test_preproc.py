c63e961b46de3d07101b7200e20f708d8e60d851
Carl Parker <carljparker@fb.com>
2021-03-09 20:39:29 -0800

    Add Makefile that shows how to invoke unit test...might delete this later

---
+++ easy_music_generator/preprocessor/tests/test_preproc.py
@@ -0,0 +1,21 @@
+import unittest
+import chord_distribution as cd
+
+class TestChordDistribution( unittest.TestCase ):
+
+    def test_get_chord_probabilities( self ):
+        #
+        # Print a blank separator line.
+        #
+        print()
+
+        chord_distro = cd.ChordDistribution()
+
+        my_chord_dict = { 'chord_1': 1, 'chord_2': 1 }
+
+        print( cd.ChordDistribution.get_chord_probabilities( my_chord_dict ) )
+        print( chord_distro.get_chord_probabilities( my_chord_dict ) )
+
+
+# --- END --- #
+
