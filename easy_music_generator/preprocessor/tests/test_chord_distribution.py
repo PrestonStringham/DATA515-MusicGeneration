137e212453269587ccfa84e01b3efaaaed1ce640
Carl Parker <carljparker@fb.com>
2021-03-10 20:39:41 -0800

    Remedy Flake8 issues...Sheesh

--- easy_music_generator/preprocessor/tests/test_chord_distribution.py
+++ easy_music_generator/preprocessor/tests/test_chord_distribution.py
@@ -1,9 +1,10 @@
 import unittest
 import preprocessor.chord_distribution as cd
 
-class TestChordDistribution( unittest.TestCase ):
 
-    def test_get_chord_probabilities( self ):
+class TestChordDistribution(unittest.TestCase):
+
+    def test_get_chord_probabilities(self):
         #
         # Print a blank separator line.
         #
@@ -11,22 +12,18 @@ class TestChordDistribution( unittest.TestCase ):
 
         chord_distro = cd.ChordDistribution()
 
-        my_chord_dict = { 'chord_1': 1, 'chord_2': 1 }
+        my_chord_dict = {'chord_1': 1, 'chord_2': 1}
 
         #
         # Test the method as a static method
         #
-        _ = cd.ChordDistribution.get_chord_probabilities( my_chord_dict )
-        self.assertEqual( my_chord_dict[ 'chord_1' ], 0.5 )
-        self.assertEqual( my_chord_dict[ 'chord_2' ], 0.5 )
+        _ = cd.ChordDistribution.get_chord_probabilities(my_chord_dict)
+        self.assertEqual(my_chord_dict['chord_1'], 0.5)
+        self.assertEqual(my_chord_dict['chord_2'], 0.5)
 
         #
         # Test the method as an instance method
         #
-        _ = chord_distro.get_chord_probabilities( my_chord_dict ) 
-        self.assertEqual( my_chord_dict[ 'chord_1' ], 0.5 )
-        self.assertEqual( my_chord_dict[ 'chord_2' ], 0.5 )
-
-
-# --- END --- #
-
+        _ = chord_distro.get_chord_probabilities(my_chord_dict)
+        self.assertEqual(my_chord_dict['chord_1'], 0.5)
+        self.assertEqual(my_chord_dict['chord_2'], 0.5)
