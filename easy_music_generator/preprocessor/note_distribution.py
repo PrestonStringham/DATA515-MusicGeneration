ac77fab58983bfbd638988dd62fa3de8f2fb8291
andreiabds <andreia.bdsodre@gmail.com>
2021-03-14 16:56:05 -0700

    formatting note distribution and preprocessor

--- easy_music_generator/preprocessor/note_distribution.py
+++ easy_music_generator/preprocessor/note_distribution.py
@@ -6,31 +6,37 @@ np.set_printoptions(threshold=sys.maxsize)
 
 
 class NoteDistribution:
+    '''
+    This class contains methods to get statistics about the notes in the melody of a song. 
+    '''
 
     def __init__(self):
         pass
 
     @staticmethod
     def get_note_matrix(scores):
+        '''
+        This method takes a list of score objects as input.
+        The output is a tuple containing the a 128x128 matrix with MIDI notes probabilities and a note probability dictionary.
+
+        OUTPUT: (128x128 matrix, {Note: probability})
+        '''
         # Dictionary to store probabilites of neighboring notes
         dic = {}
         note_dic = {}
+        
         # iterate through all the scores
         for score in scores:
-
             # The actual list of notes
             notes = score.flat.notes
             itr = iter(range(len(notes) - 1))
             # Iterate through all the notes
             for i in itr:
                 # Make sure all the notes are ACTUALLY notes...
-                if not isinstance(notes[i], mus.note.Note) or not \
-                        isinstance(notes[i+1], mus.note.Note):
+                if not isinstance(notes[i], mus.note.Note) or not isinstance(notes[i+1], mus.note.Note):
                     continue
-                # Create pairs of notes that will later be parsed
-                # for stochastic matrix
-                pair = str(notes[i].pitch.midi) + " " \
-                    + str(notes[i+1].pitch.midi)
+                # Create pairs of notes for stochastic matrix
+                pair = str(notes[i].pitch.midi) + " " + str(notes[i+1].pitch.midi)
 
                 # Add them to the dictionary
                 if pair in dic:
@@ -54,13 +60,19 @@ class NoteDistribution:
                 raise NoNotesFoundException()
 
             note_dic = NoteDistribution.get_note_probabilities(note_dic)
-            stochastic_matrix = \
-                NoteDistribution.get_stochastic_note_matrix(dic)
+            stochastic_matrix = NoteDistribution.get_stochastic_note_matrix(dic)
 
         return (stochastic_matrix, note_dic)
 
     @staticmethod
     def get_stochastic_note_matrix(distribution):
+        '''
+        This method takes a dictionary as input {Note : probability}. 
+        It ouputs a 128x128 matrix representing the probability of each MIDI note and the interaction with other notes.
+
+        OUPUT: 128x128 matrix
+        '''
+
         # 128 total MIDI notes
         size = 128
 
@@ -82,6 +94,12 @@ class NoteDistribution:
 
     @staticmethod
     def get_note_probabilities(distribution):
+        '''
+        This method takes a dictionary as input {Note : number of occurences}. 
+        It ouputs a dictionary with the probability of each note occuring.
+
+        OUTPUT:  {Note : probability}
+        '''
         total = sum(distribution.values())
         for key in distribution.keys():
             distribution[key] /= total
@@ -89,7 +107,6 @@ class NoteDistribution:
 
 
 class NoNotesFoundException(Exception):
-    def __init__(self, message="The provided MIDI or MusicXML "
-                 "files do not contain any notes."):
+    def __init__(self, message="The provided MIDI or MusicXML files do not contain any notes."):
         self.message = message
         super().__init__(self.message)
--- easy_music_generator/preprocessor/preprocessor.py
+++ easy_music_generator/preprocessor/preprocessor.py
@@ -5,21 +5,30 @@ import glob
 
 
 class Preprocessor:
-
+'''
+This class contains methods to process MIDI and/or MXL files provided by user. 
+The objective is to understand the distribution of notes and chords in the songs.
+After processing it, the output are a note probability matrix and a chord probability matrix.
+'''
     def __init__(self):
         self.scores = None
 
     def parse_scores(self, path):
+        '''
+        This method takes MIDI and MusicXML files as input and it outputs a list of score objects.
+
+        Scores are a Music21 objects that allows to represent multi-part music in a data format that is easy to work with.
+        '''
         # Look for MusicXML and MIDI Files
         mxlfiles = [f for f in glob.glob(path + "*.mxl")]
         midifiles = [f for f in glob.glob(path + "*.mid")]
         files = mxlfiles + midifiles
+        
         # If no MIDI or MusicXML files exist, raise exception
-        score_objects = []
-
         if len(files) == 0:
             raise NoFilesFoundException()
 
+        score_objects = []
         for file in files:
             score = mus.converter.parse(file)
             score_objects.append(score)
@@ -28,14 +37,19 @@ class Preprocessor:
         return self.scores
 
     def get_note_matrix(self):
+        '''
+        This method returns a matrix with the probability of occurrence of notes in a melody.
+        '''
         return NoteDistribution.get_note_matrix(self.scores)
 
     def get_chord_matrix(self):
+        '''
+        This method returns a matrix with the probability of occurrence of chords in a song.
+        '''
         return ChordDistribution.get_chord_matrix(self.scores)
 
 
 class NoFilesFoundException(Exception):
-    def __init__(self, message="No MIDI or MusicXML files found in "
-                 "the provided directory. Please check the path."):
+    def __init__(self, message="No MIDI or MusicXML files found in the provided directory. Please check the path."):
         self.message = message
         super().__init__(self.message)
