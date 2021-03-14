c21e521cf5c2b2962a2d3c4194bb7a67f983e783
Carl Parker <carljparker@fb.com>
2021-03-14 11:59:42 -0700

    Remove pre-existing files and check for existence of output files

--- easy_music_generator/tests/test_easy_music_generator.py
+++ easy_music_generator/tests/test_easy_music_generator.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import unittest
 import easy_music_generator as emg
 
@@ -7,9 +9,23 @@ class TestEasyMusicGenerator(unittest.TestCase):
     Unit tests for easy_music_generator.py
     """
 
+    def setUp(self):
+
+        import pdb
+        pdb.set_trace()
+        pre_exist = glob.glob( './output/*.mid' )
+
+        if len( pre_exist ):
+            for midi in pre_exist:
+                os.remove( midi )
+
+        if os.path.exists( "./output" ):
+            os.rmdir( "./output" )
+
     def test_easy_music_generator(self):
         emg_obj = emg.EasyMusicGenerator()
 
         emg_obj.analyze('music/')
         emg_obj.generate(140, 10, 'output/')
-        self.assertTrue(True)
+        out_midi = glob.glob( './output/*.mid' )
+        self.assertNotEqual( len( out_midi ), 0 )
