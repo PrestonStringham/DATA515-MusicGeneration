b846964bf6cfbb0c133987442f613a1cfbca73da
Carl Parker <carljparker@fb.com>
2021-03-14 12:23:35 -0700

    Remove debug code and fix flake8 issues

--- easy_music_generator/tests/test_easy_music_generator.py
+++ easy_music_generator/tests/test_easy_music_generator.py
@@ -11,21 +11,19 @@ class TestEasyMusicGenerator(unittest.TestCase):
 
     def setUp(self):
 
-        import pdb
-        pdb.set_trace()
-        pre_exist = glob.glob( './output/*.mid' )
+        pre_exist = glob.glob('./output/*.mid')
 
-        if len( pre_exist ):
+        if len(pre_exist):
             for midi in pre_exist:
-                os.remove( midi )
+                os.remove(midi)
 
-        if os.path.exists( "./output" ):
-            os.rmdir( "./output" )
+        if os.path.exists("./output"):
+            os.rmdir("./output")
 
     def test_easy_music_generator(self):
         emg_obj = emg.EasyMusicGenerator()
 
         emg_obj.analyze('music/')
         emg_obj.generate(140, 10, 'output/')
-        out_midi = glob.glob( './output/*.mid' )
-        self.assertNotEqual( len( out_midi ), 0 )
+        out_midi = glob.glob('./output/*.mid')
+        self.assertNotEqual(len(out_midi), 0)
