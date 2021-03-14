be6c94ef58518320f4d1594bf710cac0c0e2ef82
HasnahSaid <hasna024@uw.edu>
2021-03-14 14:45:58 -0700

    Fixed flake8 issues in unit tests

--- easy_music_generator/preprocessor/tests/test_chord_distribution.py
+++ easy_music_generator/preprocessor/tests/test_chord_distribution.py
@@ -1,13 +1,11 @@
 import unittest
 import preprocessor.chord_distribution as cd
-import numpy as np
 import music21 as mus
 
 
 class TestChordDistribution(unittest.TestCase):
     """
     Unit tests for chord_distribution.py, a preprocessor module
-    
     """
     def test_get_chord_matrix(self):
         '''
@@ -15,41 +13,36 @@ class TestChordDistribution(unittest.TestCase):
         and returns the prbability dictionary
         '''
 
-        # Create a Score object with two chords to process and get probability 
+        # Create a Score object with two chords to process and get probability
         s = mus.stream.Score()
         c1 = mus.chord.Chord(['D', 'F#', 'A'])
         c2 = mus.chord.Chord('A4 C#5 E5')
         s.append(c1)
         s.append(c2)
 
-        expected_output = {'D': 1.0}
+        expected_output = {'D': 0.5, 'A': 0.5}
         actual_output = cd.ChordDistribution.get_chord_matrix([s])
         self.assertEqual(expected_output, actual_output)
 
-
-
     def test_get_chord_matrix_chordify(self):
         '''
-        Unit tests get_chord_matrix() when the score object doesn't have chords. 
-        Mainly to test chordify files
+        Unit tests get_chord_matrix() when the score object doesn't
+        have chords. Mainly to test chordify files
         '''
-        
         # Score object that doesn't contain any chords
         no_chord_score_obj = mus.stream.Score()
         no_chord_score_obj.append(mus.note.Note('G9'))
-        no_chord_score_obj.append(mus.note.Note('F9')) 
-
-        expected_output = {'Gpedal': 1.0}
+        no_chord_score_obj.append(mus.note.Note('F9'))
+        expected_output = {'Gpedal': 0.5, 'Fpedal': 0.5}
 
         chord_distro = cd.ChordDistribution()
-        self.assertEqual(expected_output, chord_distro.get_chord_matrix([no_chord_score_obj]))
-
-
+        actual_output = chord_distro.get_chord_matrix([no_chord_score_obj])
+        self.assertEqual(expected_output, actual_output)
 
     def test_get_chord_matrix_raise_no_note_exception(self):
         '''
-        Test that get_chord_matrix() raises an exception when the score object it gets
-        does not contain any chords
+        Test that get_chord_matrix() raises an exception when the score
+        object it gets does not contain any chords
         '''
 
         # Create an empty Score object to raise the exception
@@ -61,16 +54,13 @@ class TestChordDistribution(unittest.TestCase):
             context.exception.message,
             "The provided MIDI or MusicXML files do not contain any chords.")
 
-   
-
     def test_get_chord_probabilities(self):
         '''
-        Test get_chord_probabilities(), the function takes a dictionary of chord counts
-        and returns a dictionary of the chord probabilities
+        Test get_chord_probabilities(), the function takes a dictionary
+        of chord counts and returns a dictionary of the chord probabilities
         '''
 
         my_chord_dict = {'chord_1': 1, 'chord_2': 1}
-  
         # Test the method as a static method
         _ = cd.ChordDistribution.get_chord_probabilities(my_chord_dict)
         self.assertEqual(my_chord_dict['chord_1'], 0.5)
@@ -81,5 +71,3 @@ class TestChordDistribution(unittest.TestCase):
         _ = chord_distro.get_chord_probabilities(my_chord_dict)
         self.assertEqual(my_chord_dict['chord_1'], 0.5)
         self.assertEqual(my_chord_dict['chord_2'], 0.5)
-
-
