b26d5829a48256a7fc15b20cbc43a0026a3c2a06
HasnahSaid <hasna024@uw.edu>
2021-03-16 14:35:11 -0700

    Added check for empty notes dictionary

--- easy_music_generator/preprocessor/tests/test_note_distribution.py
+++ easy_music_generator/preprocessor/tests/test_note_distribution.py
@@ -43,7 +43,7 @@ class TestNoteDistribution(unittest.TestCase):
         Test that get_note_matrix() raises an exception when the score
         object it gets does not contain any notes
         '''
-        score = mus.converter.parse('./easy_music_generator/music/test_no_notes_exception.xml')
+        score = mus.stream.Score()        
         with self.assertRaises(nd.NoNotesFoundException) as context:
             nd.NoteDistribution.get_note_matrix([score])
 
